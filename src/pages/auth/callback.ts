5660ea9987995d51d9a5ec5ffa72a8fa8ebe5831
Michael Chan <mijoch@gmail.com>
2024-08-05 16:07:43 -0700

    parse code from auth/callback request

--- src/pages/auth/callback.ts
+++ src/pages/auth/callback.ts
@@ -1,7 +1,17 @@
 import type {APIRoute} from 'astro';
 
-export const GET: APIRoute = async () => {
-	return new Response(`Auth callback reached.`);
+export const GET: APIRoute = async ({request}) => {
+	const code = new URL(request.url).searchParams.get('code');
+
+	if (!code) {
+		return new Response('No code found in the URL', {
+			status: 400,
+		});
+	}
+
+	return new Response(
+		`Auth callback reached with code: ${code}`
+	);
 };
 
 export const prerender = false;
