a844943d8d22df5f6c1fceab2dcaf6760f53e991
Michael Chan <mijoch@gmail.com>
2024-08-05 14:55:54 -0700

    get authkit authorization url

--- src/pages/sign-in.ts
+++ src/pages/sign-in.ts
@@ -1,7 +1,17 @@
 import type {APIRoute} from 'astro';
+import {WorkOS} from '@workos-inc/node';
 
 export const GET: APIRoute = async () => {
-	return new Response('Hello World!');
+	const workos = new WorkOS(import.meta.env.WORKOS_API_KEY);
+
+	const authorizationURL =
+		workos.userManagement.getAuthorizationUrl({
+			provider: 'authkit',
+			redirectUri: 'http://localhost:4321/auth/callback',
+			clientId: import.meta.env.WORKOS_CLIENT_ID,
+		});
+
+	return new Response(authorizationURL);
 };
 
 export const prerender = false;
