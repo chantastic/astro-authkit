5b42820369124e58cde8634b735729b40460f0b8
Michael Chan <mijoch@gmail.com>
2024-08-06 09:08:10 -0700

    add withAuth middleware function (boilerplate)

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,11 +1,22 @@
+import type {APIContext, MiddlewareNext} from 'astro';
 import {defineMiddleware} from 'astro:middleware';
 
 export const onRequest = defineMiddleware((context, next) => {
 	const {pathname} = new URL(context.request.url);
 
 	if (pathname.startsWith('/dashboard')) {
-		console.log(`Middleware running for ${pathname}.`);
+		return withAuth(context, next);
 	}
 
 	return next();
 });
+
+async function withAuth(
+	context: APIContext,
+	next: MiddlewareNext
+) {
+	console.log(`withAuth Middleware running.`);
+
+	const response = await next();
+	return response;
+}
