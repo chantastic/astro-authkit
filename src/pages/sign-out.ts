475118ef5c661740b0a03b0526e5d1edfde9b61e
Michael Chan <mijoch@gmail.com>
2024-08-05 22:07:51 -0700

    get 'wos-session' cookie; redirect in none

--- src/pages/sign-out.ts
+++ src/pages/sign-out.ts
@@ -1,7 +1,13 @@
 import type {APIRoute} from 'astro';
 
-export const GET: APIRoute = async () => {
-	return new Response('Signing out…');
+export const GET: APIRoute = async ({cookies, redirect}) => {
+	const cookie = cookies.get('wos-session');
+
+	if (!cookie?.value) {
+		return redirect('/sign-in');
+	}
+
+	return new Response(`Signing out…`);
 };
 
 export const prerender = false;
