cfdb91ba2605a6005bcfb01ee767214501db8222
Michael Chan <mijoch@gmail.com>
2024-08-06 08:51:16 -0700

    restrict middleware from running, save for protected routes

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,7 +1,11 @@
 import {defineMiddleware} from 'astro:middleware';
 
-export const onRequest = defineMiddleware((_, next) => {
-	console.log('Middleware running…');
+export const onRequest = defineMiddleware((context, next) => {
+	const {pathname} = new URL(context.request.url);
+
+	if (pathname.startsWith('/dashboard')) {
+		console.log(`Middleware running for ${pathname}.`);
+	}
 
 	return next();
 });
